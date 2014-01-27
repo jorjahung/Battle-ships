a7b7b86ad0cdc19114989827c74524ffc2232516
Berta-G <berta.gutierrez@gmail.com>
2014-01-27 11:46:21 +0000

    class Player and rspec working

--- Lib/player.rb
+++ Lib/player.rb
@@ -1,10 +1,19 @@
 class Player
 	def initialize(name)
 		@name = name
+		@board = Board.new(self)
 	end
 
+	attr_accessor :board
 	def name
 		@name
 	end
 
+	def has_ships_still_floating?
+		board.rows.include?("s")
+	end
+
+	def shoot(at_coordinates, opponents_board)
+		opponents_board.register_shot at_coordinates
+	end
 end
--- Spec/player_spec.rb
+++ Spec/player_spec.rb
@@ -2,12 +2,32 @@ require './Lib/player'
 
 describe Player do
 	let(:player) {Player.new("Myname")}
-	let(:board) {double :board}
+	#let(:board) {Board.new(player)}
 
 	it "is initialized with its name" do
 		player = Player.new("Myname")
 		expect(player.name).to eq("Myname")
 	end
 
+	it "creates a new board when initialized" do
+		expect(player.board.owner).to eq(player.name)
+	end
+
+	it "should have ships still floating when created" do
+		Board.any_instance.stub(:rows => Array.new(100, 's'))
+		expect(player).to have_ships_still_floating
+	end
+
+	it "should not have ships still floating when all down" do
+		Board.any_instance.stub(:rows => Array.new(100, 'x'))
+		expect(player).not_to have_ships_still_floating
+	end
+
+	it "should call the method register_shot of the other_board" do
+		other_board =  double :board
+		expect(other_board).to receive(:register_shot)
+		player.shoot("A1", other_board)
+		
+	end	
 
 end
