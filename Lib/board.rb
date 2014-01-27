6a546b2628106cbc65cc5d2c94a355aa94a4bc86
Peter Saxton <peterhsaxton@gmail.com>
2014-01-27 10:23:55 +0000

    Added required methods to Board class

---
+++ Lib/board.rb
@@ -0,0 +1,25 @@
+class Board
+
+	def initialize(player)
+		@player = player
+	end
+
+	attr_reader :player
+
+	def owner
+		player.name
+	end
+
+	def register_shot at_coordinates
+		
+	end
+
+	def rows
+		
+	end
+
+	def opponent_view
+
+	end
+
+end
